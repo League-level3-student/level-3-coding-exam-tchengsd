ca091fcafa78f6a122a155e20c19f4f70d3f0a33
league <league@imac-24.attlocal.net>
2020-01-04 14:24:23 -0800

    Started Test

--- src/VoteProcessor.java
+++ src/VoteProcessor.java
@@ -1,4 +1,23 @@
+import java.util.ArrayList;
 
 public class VoteProcessor {
-
+	String calculateElectionWinner(ArrayList<String> votes) {
+		int votesP = 0;
+		int votesE = 0;
+		for(int i = 0; i < votes.size(); i++) {
+			if(votes.get(i).equalsIgnoreCase("pope francis")) {
+				votesP++;
+			} else if(votes.get(i).equalsIgnoreCase("edward snowden")) {
+				votesE++;
+			}
+		}
+		if(votesP > votesE) {
+			return "pope francis";
+		} else if (votesE > votesP) {
+			return "edward snowden";
+		} else if (votesP == votesE) {
+			return "TIE";
+		}
+		return "";
+	}
 }
